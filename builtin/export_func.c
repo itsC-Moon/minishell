1d3b1c6738101d862c68e498cbee502d2faaef5a
hichambenoukaiss0@gmail.com <hibenouk@e1r11p4.1337.ma>
2024-03-25 14:09:17 +0000

    git get crazy sometime

--- builtin/export_func.c
+++ builtin/export_func.c
@@ -6,15 +6,116 @@
 /*   By: zkotbi <student.h42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 03:23:52 by zkotbi            #+#    #+#             */
-/*   Updated: 2024/03/25 14:05:19 by hibenouk         ###   ########.fr       */
+/*   Updated: 2024/03/25 14:08:55 by hibenouk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "libft.h"
 
+#include "minishell.h"
+#include "libft.h"
+
 static t_lst		*find_var(t_env	*env, const char *name)
 {
-	// (void)proc;
+	t_lst	*tmp;
+	int		i;
+
+	tmp = env->front;
+	while(tmp != NULL)
+	{
+		i = 0;
+		while (tmp->varible[i] == name[i] && name[i] != '=')
+			i++;
+		if (tmp->varible[i] == '=' && name[i] == '=')
+			return (tmp);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
+static int is_valide(const char *buffer)
+{
+	if (*buffer == '=' || ft_isdigit(*buffer))
+		return (0);
+	while (*buffer && is_id(*buffer))
+		buffer++;
+	if (*buffer == '=')
+		return (1);
+	else if (*buffer == '\0')
+		return (2);
+	return (0);
+}
+
+static void new_var(const char *buffer, t_env *env)
+{
+	t_lst	*ptr;
+
+	ptr = find_var(env, buffer);
+	if (!ptr)
+	{
+		lst_addback(env->back, buffer);
+		env->size++;
+	}
+	else
+	{
+		free(ptr->varible);
+		ptr->varible = ft_strdup(buffer);
+	}
+}
+
+static int export_var(t_proc *proc, t_env *env)
+{
+	size_t	i;
+	int		status;
+	int		state;
+
+	status = 0;
+	i = 0;
+	while (i < proc->nb_args - 1)
+	{
+		state = is_valide(proc->args[i + 1]);
+		if (state == 1)
+			new_var(proc->args[i + 1], env);
+		else if (state == 0)
+		{
+			status = 1;
+			ft_printf(2, "nudejs: export: `%s': not a valid identifier\n",\
+					proc->args[i + 1]);
+		}
+		i++;
+	}
+	return (status);
+}
+
+static void print_env2(t_env *env, int fd)
+{
+	const t_lst *it;
+	const char	*it2;
+
+	it = env->front;
+	while (it)
+	{
+		ft_printf(fd, "declare -x ");
+		it2 = it->varible;
+		while (*it2 && *it2 != '=')
+			write(fd, it2++, 1);
+		if (*it2)
+			ft_printf(fd, "=\"%s\"\n",it2 + 1);
+		it = it->next;
+	}
+}
+int export_func(t_proc	*proc, int *fd, t_env *env)
+{
+
+	if (open_builtin_files(proc) == 1)
+		return (1);
+	if (fd != NULL)
+		get_pipe_io_files(proc, fd);
+	else
+		get_io_files(proc);
+	if (proc->nb_args == 1)
+		print_env2(env, proc->io_fd[1]);
+	else
+		return (export_var(proc, env));
 	return (0);
 }
