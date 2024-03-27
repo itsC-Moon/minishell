19ca9ce1dae7efcc357fb108c6a54438efeddb64
Zaid Kotbi <zkotbi@e3r4p13.1337.ma>
2024-03-27 03:15:46 +0000

    fix norm & some bug

--- execution/path_handle.c
+++ execution/path_handle.c
@@ -6,19 +6,18 @@
 /*   By: hibenouk <hibenouk@1337.ma>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 00:30:19 by hibenouk          #+#    #+#             */
-/*   Updated: 2024/03/25 03:02:23 by zkotbi           ###   ########.fr       */
+/*   Updated: 2024/03/27 03:04:11 by zkotbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "libft.h"
 #include "minishell.h"
-#include <unistd.h>
+#include <stdio.h>
 
-static char **get_paths(t_env	*env)
+static char	**get_paths(t_env	*env)
 {
-	char *path;
-	char **paths;
+	char	*path;
+	char	**paths;
 
 	path = env_search(env, "PATH");
 	if (path == NULL)
@@ -29,31 +28,40 @@ static char **get_paths(t_env	*env)
 	return (paths);
 }
 
-static char *get_path(char **paths, char *cmd)
+static int	get_path_loupe(char ***paths, char **tmp_cmd,
+		char **tmp_path, char **path)
 {
-	char *tmp_cmd;
-	char *path;
-	char *tmp_path;
-	int i;
+	int	i;
 
 	i = 0;
-	tmp_path = NULL;
-	path = ft_strdup("");
-	tmp_cmd = ft_strjoin("/", cmd);
-	while (access(path, F_OK | X_OK) == -1)
+	while (access(*path, F_OK | X_OK) == -1)
 	{
-		if (access(path, F_OK) == 0)
+		if (access(*path, F_OK) == 0)
 		{
-			free(tmp_path);
-			tmp_path = path;
+			free(*tmp_path);
+			*tmp_path = *path;
 		}
 		else
-			free(path);
-		if (paths[i] == NULL)
-			break;
-		path =  ft_strjoin(paths[i], tmp_cmd);
+			free(*path);
+		if ((*paths)[i] == NULL)
+			break ;
+		*path = ft_strjoin((*paths)[i], *tmp_cmd);
 		i++;
 	}
+	return (i);
+}
+
+static char	*get_path(char **paths, char *cmd)
+{
+	char	*tmp_cmd;
+	char	*path;
+	char	*tmp_path;
+	int		i;
+
+	tmp_path = NULL;
+	path = NULL;
+	tmp_cmd = ft_strjoin("/", cmd);
+	i = get_path_loupe(&paths, &tmp_cmd, &tmp_path, &path);
 	if (paths[i] == NULL && tmp_path != NULL)
 		return (free(tmp_cmd), tmp_path);
 	else if (paths[i] == NULL)
@@ -61,29 +69,30 @@ static char *get_path(char **paths, char *cmd)
 	return (free(tmp_cmd), free(tmp_path), path);
 }
 
-static char *null_path_handle(t_proc *proc)
+static char	*null_path_handle(t_proc *proc)
 {
-	char *pwd;
-	char *cmd;
-	char *tmp;
+	char	*pwd;
+	char	*cmd;
+	char	*tmp;
 
 	pwd = getcwd(NULL, 0);
+	if (pwd == NULL)
+		return (NULL);
 	cmd = ft_strjoin("/", proc->args[0]);
 	tmp = ft_strjoin(pwd, cmd);
 	return (free(cmd), free(pwd), tmp);
 }
 
-char *get_cmd_path(t_proc	*proc, t_env *env)
+char	*get_cmd_path(t_proc	*proc, t_env *env)
 {
-	char **paths;
-	char *cmd;
+	char	**paths;
+	char	*cmd;
 
 	paths = get_paths(env);
 	if (proc->args[0][0] == 0 || ft_strchr(proc->args[0], '/') != NULL)
-		return (free_tab(paths), ft_strdup(proc->args[0])); // TODO : free paths;
+		return (free_tab(paths), ft_strdup(proc->args[0]));
 	if (paths == NULL || paths[0][0] == 0)
 		return (free_tab(paths), null_path_handle(proc));
 	cmd = get_path(paths, proc->args[0]);
-	return (free_tab(paths), cmd); // TODO : free paths;
+	return (free_tab(paths), cmd);
 }
-
---
+++ execution/pipe_utils.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkotbi <hibenouk@1337.ma>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 03:08:51 by zkotbi            #+#    #+#             */
+/*   Updated: 2024/03/27 03:09:46 by zkotbi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+#include "libft.h"
+
+t_pipe	*init_pipe_struct(int size)
+{
+	t_pipe	*pipe;
+
+	pipe = malloc(sizeof(t_pipe));
+	check_null(pipe, "malloc");
+	pipe->i = 0;
+	pipe->fd[0] = 0;
+	pipe->fd[1] = 0;
+	pipe->tmp[0] = -1;
+	pipe->tmp[1] = -1;
+	pipe->pids = malloc(sizeof(int) * size);
+	check_null(pipe->pids, "malloc");
+	return (pipe);
+}
